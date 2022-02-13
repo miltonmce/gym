4bde5dee401ffaf222c1bf72506b4d40e36f6e95
Milton Medina <miltonmce@gmail.com>
2022-02-13 12:03:49 -0700

    first commit

---
+++ management/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
